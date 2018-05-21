18ea7abab6f03b20d61955d21edfc159e320f6c6
Austin <austin.griffith@madwiremedia.com>
2018-05-21 09:45:52 -0400

    make that front page look just a touch better

--- src/readme.sh
+++ src/readme.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-cat ../README.md | marked -o readme.html
+cat ../README.md | marked -o temp.html
+echo "<html><body style='font-family:-apple-system,BlinkMacSystemFont,Helvetica,Arial,sans-serif;padding:20px'>" > readme.html
+cat temp.html >> readme.html
+echo "</body></html>" >> readme.html
